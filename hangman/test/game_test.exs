444bf68c4441dc7f201b6e1fcab5aad2f444c7ce
hungphi98 <nguyenhungphi2698@Gmail.com>
2019-10-19 14:52:31 -0400

    hangman

---
+++ hangman/test/game_test.exs
@@ -0,0 +1,48 @@
+defmodule GameTest do
+  use ExUnit.Case
+  doctest Hangman
+  alias Hangman.Game
+
+  test "new_game" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "state isn't changed for :won game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "test a good guess i recognizable" do
+    game = Game.new_game("wibble")
+    {game, _tally} = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "test a bad guess is recognizable" do
+    game = Game.new_game("wibble")
+    {game, _tally} = Game.make_move(game, "a")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+end
